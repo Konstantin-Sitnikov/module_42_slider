b23c91cc153e6859f6a4df4c65d1daa377475d19
Konstantin-Sitnikov <robokot_90@mail.ru>
2025-10-21 16:19:50 +0900

    next

--- slider.js
+++ slider.js
@@ -3,121 +3,73 @@ const data = [
 	{
 		"id":1,
 		"city": "Rostov-on-Don",
-		"district": "LCD admiral",
+		"district": "Admiral",
 		"apartment area": 81,
 		"repair time": 3.5,
-		"repair rost:": "Upon request",
-		"img sqr": ""
+		"repair rcost": "Upon request",
+		"img sqr": "images/Rostov-on-Don_admiral.png"
 	},
+
 	 {
+		"id": 2,
+		"city": "Sochi",
+		"district": "Thieves",
+		"apartment area": 105,
+		"repair time": 4,
+		"repair rcost": "Upon request",
+		"img sqr": "images/Sochi.png"
+	}, 
 
-	}, {
-
+	{
+		"id": 3,
+		"city": "Rostov-on-Don",
+		"district": "Patriotic",
+		"apartment area": 93,
+		"repair time": 3,
+		"repair rcost": "Upon request",
+		"img sqr": "images/Rostov-on-Don_patriotic.png"
 	}
 ]
 
 
-
-
-
-
-
-
 const chatBody = document.querySelector(".chat__container--body")
 
-const connection = new WebSocket('wss://echo.websocket.org');
 
+function getInfoToHtml (object) {
 
-connection.addEventListener("open", (event) => {
+	const contentTextCity = document.querySelector(".content__text--city")
+	const contentTextArea = document.querySelector(".content__text--area")
+	const contentTextRepairTime = document.querySelector(".content__text--repair-time")
+	const contentTextRepairRcost = document.querySelector(".content__text--repair-rcost")
+	const contentContainerImg = document.querySelector(".content__container--img")
 
-	chatBody.innerHTML += 
-		`<span class="chat__text chat__text--servise-message">Соединение установлено.</span>`
+	contentTextCity.innerHTML = `${object.city}<br>${object.district}`
+	contentTextArea.innerHTML = `${object["apartment area"]}`
+	contentTextRepairTime.innerHTML = `${object["repair time"]}`
+	contentTextRepairRcost.innerHTML = `${object["repair rcost"]}`
+	contentContainerImg.innerHTML = `<img src="${object['img sqr']}" alt="foto" class="content__img">` 
 
-})
-
-
-connection.addEventListener("error", () => {
-
-	chatBody.innerHTML += 
-		`<span class="chat__text chat__text--servise-message">Что то пошло не так!!!</span>`
-
-})
-
-connection.addEventListener("message", (event) => {
-	
-	if (event.data.includes("Request")) {
-		return
-	}
-	
-
-	chatBody.innerHTML += 
-		`<div class="chat__container chat__container--message chat__container--server">
-		<span class="chat__text chat__text--message">${event.data}</span></div>`
-
-	resizeMessage()
-
-})
-
-
-function resizeMessage() {
-
-	const message = chatBody.lastElementChild
-	const messageText = message.firstElementChild
-
-	if (messageText.offsetWidth >= (chatBody.clientWidth * 0.7)) {
-	 messageText.style.width = `${chatBody.clientWidth * 0.6}px`
-	} 
-}
-
-function sendMesaage() {
-	const input = document.querySelector(".chat__input")
-
-	if (input.value === "") {
-		return
-	}
-
-	chatBody.innerHTML += 
-		`<div class="chat__container chat__container--message chat__container--user">
-		<span class="chat__text chat__text--message">${input.value}</span></div>`
-		
-
-	resizeMessage()
-
-
-	connection.send(input.value)
-	input.value = ""
-	}
-
-
-const okPosition = (positon) => {
-	const latitude = positon.coords.latitude
-	const longitude = positon.coords.longitude
-	chatBody.innerHTML += 
-		`<div class="chat__container chat__container--message chat__container--user">
-            <a href="https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}#map=8/${latitude}/${longitude}" class="chat__link">Вы тут</a>
-         </div>`
 
 }
 
-const errorPosition = () => {
-	chatBody.innerHTML += 
-		`<span class="chat__text chat__text--servise-message">Ошибка при получении позиции!</span>`
+function getNavAndMarkerToHtml(list) {
+	const contentContainerNav = document.querySelector(".content__container--nav")
+	const contentContainerMarkers = document.querySelector(".content__container--markers")
 
-}
-
-
-function geoLocation() {
-
-	if (!navigator.geolocation) {
-
-		chatBody.innerHTML += 
-		`<span class="chat__text chat__text--servise-message">Ваш браузер не потдерживает геолокацию</span>`
-
-	} else {
-		chatBody.innerHTML += 
-		`<span class="chat__text chat__text--servise-message">Определяем местоположение!</span>`
-		navigator.geolocation.getCurrentPosition(okPosition, errorPosition)
+	for (obj of list) {
+		contentContainerNav.innerHTML += `<span data-id="${obj.id}" class="content__header content__header--nav">${obj.city}, ${obj.district}</span>`
+		contentContainerMarkers.innerHTML += `<div data-id="${obj.id}" class="content__container content__container--marker">
+                            
+												<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<rect width="9.56667" height="9.56667" rx="4.78333" fill="white" fill-opacity="0.3"/>
+												</svg>
+											 </div>`
 	}
-	
-	
+
+
 }
+
+getInfoToHtml(data[0])
+
+getNavAndMarkerToHtml(data)
+
